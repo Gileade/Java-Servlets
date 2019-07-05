9fad261e53e21931501a8d2be4cf672ebbd5b8dd
Gileade Trindade Silva <gileade@Gile>
2019-07-04 21:19:35 -0300

    Add OlaMundo.Java

---
+++ gerenciador/src/br/com/gile/gerenciador/servlet/OlaMundo.java
@@ -0,0 +1,28 @@
+package br.com.gile.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//Deve ser inserido essa annotation para saber onde sera acessada na URL
+@WebServlet(urlPatterns="/oi")
+public class OlaMundo extends HttpServlet{
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		//Cria um objeto PrintWriter do objeto de resposta para que possa escrever o HRML
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("Olá Mundo em Servlet");
+		out.println("</body>");
+		out.println("</html>");
+		
+		System.out.println("O servlet Olá mundo foi chamado");
+	}
+	
+}
