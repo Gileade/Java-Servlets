8603225d7bf4ea005d108b458201dbf4cea09b78
Gileade Trindade Silva <gileade.trindade@gmail.com>
2019-07-29 13:30:15 -0300

    Adicionando JSPs e alterando para a utilização

--- gerenciador/src/br/com/gile/gerenciador/servlet/ListaEmpresaServlet.java
+++ gerenciador/src/br/com/gile/gerenciador/servlet/ListaEmpresaServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -25,20 +26,12 @@ public class ListaEmpresaServlet extends HttpServlet {
 			throws ServletException, IOException {
 
 		Banco banco = new Banco();
-		List<Empresa> empresas = banco.getEmpresas();
-		PrintWriter out = response.getWriter();
+		List<Empresa> lista = banco.getEmpresas();
 
-		out.println("<html>");
-		out.println("<body>");
-		out.println("<ul>");
-		
-		for (Empresa empresa : empresas) {
-			out.println("<li>" + empresa.getNome() + "</li>");
-		}
-		
-		out.println("</ul>");
-		out.println("</body>");
-		out.println("</html>");
+		request.setAttribute("empresas", lista);
+
+		RequestDispatcher rd = request.getRequestDispatcher("/listaEmpresas.jsp");
+		rd.forward(request, response);
 	}
 
 }
--- gerenciador/src/br/com/gile/gerenciador/servlet/NovaEmpresaServlet.java
+++ gerenciador/src/br/com/gile/gerenciador/servlet/NovaEmpresaServlet.java
@@ -3,6 +3,7 @@ package br.com.gile.gerenciador.servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -24,21 +25,20 @@ public class NovaEmpresaServlet extends HttpServlet {
 	// usar esse método como post e não mais passando parametros pela URL (GET)
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+
 		System.out.println("Cadastrando nova Empresa");
 		String nomeDaEmpresa = request.getParameter("nome");
-		
+
 		Empresa empresa = new Empresa();
 		empresa.setNome(nomeDaEmpresa);
-		
+
 		Banco banco = new Banco();
 		banco.adiciona(empresa);
 
-		PrintWriter out = response.getWriter();
-		out.println("<html>");
-		out.println("<body>");
-		out.println("Empresa " + nomeDaEmpresa + " cadastrada com sucesso!");
-		out.println("</body>");
-		out.println("</html>");
+		// Chamar o JSP
+		RequestDispatcher rd = request.getRequestDispatcher("/novaEmpresaCriada.jsp");
+		request.setAttribute("empresa", empresa.getNome());
+		rd.forward(request, response);
 	}
 
 }
