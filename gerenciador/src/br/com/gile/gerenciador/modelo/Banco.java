bd26f614fef03a2cc6e656c3fc0101ebf23064ab
Gileade Trindade Silva <gileade.trindade@gmail.com>
2019-08-18 00:59:36 -0300

    Populando dados ficticios no banco

--- gerenciador/src/br/com/gile/gerenciador/modelo/Banco.java
+++ gerenciador/src/br/com/gile/gerenciador/modelo/Banco.java
@@ -5,18 +5,32 @@ import java.util.Iterator;
 import java.util.List;
 
 public class Banco {
-	private static List<Empresa> lista = new ArrayList<Empresa>();
+	private static List<Empresa> lista = new ArrayList<>();
+	private static List<Usuario> listaUsuarios = new ArrayList<>();
 	private static Integer chaveSequencial = 1;
-
+	
 	static {
 		Empresa empresa = new Empresa();
 		empresa.setId(chaveSequencial++);
 		empresa.setNome("Alura");
+		
 		Empresa empresa2 = new Empresa();
 		empresa2.setId(chaveSequencial++);
 		empresa2.setNome("Caelum");
+		
 		Banco.lista.add(empresa);
 		Banco.lista.add(empresa2);
+		
+		Usuario u1 = new Usuario();
+		u1.setLogin("gile");
+		u1.setSenha("1234");
+		
+		Usuario u2 = new Usuario();
+		u1.setLogin("lucas");
+		u1.setSenha("1234");
+		
+		Banco.listaUsuarios.add(u1);
+		Banco.listaUsuarios.add(u2);
 	}
 
 	public void adiciona(Empresa empresa) {
